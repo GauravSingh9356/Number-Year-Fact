997554401c1f0929e890d53e9ca385c0023518af
GauravSingh75 <55052983+GauravSingh9356@users.noreply.github.com>
2020-06-09 14:06:25 +0530

    Version 1.0.0

---
+++ main.js
@@ -0,0 +1,35 @@
+let fact = $('#fact');
+let factText = $('#factText');
+
+let numberInput = $('#numberInput');
+numberInput.on('keyup', function (e) {
+  $('#h4').empty();
+  if ($('#check1').is(':checked')) {
+    $('#check2').prop('checked', false);
+    $.ajax({
+      method: 'GET',
+      url: 'http://numbersapi.com/' + e.target.value,
+      dataType: 'text',
+    }).done(function (data) {
+      if (e.target.value != '') {
+        fact.css('display', 'block');
+        factText.text(data);
+      }
+    });
+  } else if ($('#check2').is(':checked')) {
+    $('#check1').prop('checked', false);
+    $.ajax({
+      method: 'GET',
+      url: 'http://numbersapi.com/' + e.target.value + '/year',
+      dataType: 'text',
+    }).done(function (data) {
+      if (e.target.value != '') {
+        fact.css('display', 'block');
+        factText.text(data);
+      }
+    });
+  }
+  $('#h4').append(
+    '<a href="https://github.com/gauravsingh9356" target="_blank">GitHub</a>'
+  );
+});
